1ac1931bbc9fe0116016d10b18aa1c80df421878
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-01-29 16:43:55 +0900

    D-2
    s-2#1
    ---
    1. "[スマホ世代でも分かるMacの基本的な使い方＆Xcodeをインストールする手順 (4/4)"
    
    	==> done

---
+++ D-2_s-1-2_Playground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+print("ac")
+
+var 果物 = 125
+print(果物)
---
+++ D-2_s-2#1.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+let abc = "aaaa"
+
+abc = "bbbb"
