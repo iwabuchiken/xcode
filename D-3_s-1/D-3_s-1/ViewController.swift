57abd936519224f1506bcd645671facaddf2dca0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-01-29 21:06:13 +0900

    D-3
    s-1#1
    ---
    1. first try
    
    	==> done

---
+++ D-3_s-1/D-3_s-1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  D-3_s-1
+//
+//  Created by mac on 2016/01/29.
+//  Copyright © 2016年 livetill150. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func tapHandler(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
