c2adba6831a5c77d8fab01d89eee7c013ef99f05
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-02-04 16:45:07 +0900

    D-4
    s-4#2
    ---
    1. L-30 => 日記アプリ
    2. delete cell
    
        ==> DROP

--- Chapter6/Chapter6/ViewController.swift
+++ Chapter6/Chapter6/ViewController.swift
@@ -13,19 +13,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //ref http://stackoverflow.com/questions/17735182/could-not-find-any-information-for-class-named-viewcontroller answered Dec 20 '14 at 9:02
     @IBOutlet weak var tableView: UITableView!
     
+    var dataArray: [Int] = []
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        //return 10
+        return dataArray.count
+    
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = "Row \(indexPath.row)!!"
+//        cell.textLabel?.text = "Row \(indexPath.row)!!"
+        cell.textLabel?.text = "\(indexPath.row)"
         
-        cell.detailTextLabel?.text = "Subtitle \(indexPath.row)"
+        //cell.detailTextLabel?.text = "Subtitle \(indexPath.row)"
+        //ref http://stackoverflow.com/questions/32552336/generating-random-numbers-with-swift-2 answered Sep 13 '15 at 17:26
+        //cell.detailTextLabel?.text = "Subtitle \( Int(arc4random_uniform(3)) )"
+        cell.detailTextLabel?.text = "Subtitle \( dataArray[indexPath.row] )"
         
         return cell
 
+    }
+    
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            dataArray.removeAtIndex(indexPath.row)
+            
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+        }
+    }
+    
+    @IBAction func add(sender: UIBarButtonItem) {
+    
+        dataArray.append(dataArray.count)
+        
+        tableView.reloadData()
+        
+        
     }
     
     override func viewDidLoad() {
