d3aeb1890976ecbbcc4d7c7e47edf8f4a836f7a3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-04 21:37:09 +0900

    xcode_Player         D-7
            s-1#2/steps.2
            ---
            1. PlayHist.swift <-- new
    
                ==> done

---
+++ Chapter-7_L-43_after/AVPlayer/AVPlayer/PH.swift
@@ -0,0 +1,47 @@
+//
+//  PH.swift
+//  avplayer
+//
+//  Created by mac on 2016/03/04.
+//  Copyright © 2016年 shoeisha. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// PH --> Play history
+
+class PH: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    //
+    dynamic var created_at = ""
+    
+    //
+    dynamic var modified_at = ""
+    
+    dynamic var audio_id = ""
+    
+    dynamic var title = ""
+
+    // memo --> memo attached to the clip
+    dynamic var memo = ""
+    
+    dynamic var current_time    = 0
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    static func show_ClassName() ->Void {
+        
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] PH class")
+        
+    }
+    
+    
+}
