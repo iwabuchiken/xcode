c31a774239e65331bab84e676da3004ac6b92c5e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-03-29 10:39:43 +0900

    xcode_Memo D-16(m)
    s-3#2/steps.1
    ---
    1. setup => location manager
    
            ==> done

--- Chapter6_L-32_after/I/chapter6/VC_ShowLoc.swift
+++ Chapter6_L-32_after/I/chapter6/VC_ShowLoc.swift
@@ -10,9 +10,17 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class VC_ShowLoc: UIViewController {
+class VC_ShowLoc: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     // MARK: vars
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var lastLocation:CLLocationCoordinate2D?
+
+
+    // loc
     var loc : Loc?
 //    var longi : Double! = 0;
 //    var lat : Double! = 0;
@@ -22,10 +30,56 @@ class VC_ShowLoc: UIViewController {
         
         super.viewDidLoad()
         
-        
+        // setup --> map
+        self._viewDidLoad()
         
     }
     
+    func _viewDidLoad() {
+        
+        locationManager.delegate = self
+        
+        // 100m 移動したら位置情報を更新する
+        locationManager.distanceFilter = 100.0
+        
+        // 測位の精度を 100ｍ とする
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        // 位置情報サービスへの認証状態を取得する
+        let status = CLLocationManager.authorizationStatus()
+        
+        //debug
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] status.rawValue => \(status.rawValue)")
+        
+        //debug
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] CLAuthorizationStatus.NotDetermined => \(CLAuthorizationStatus.NotDetermined.rawValue)")
+        
+        //debug
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] status == CLAuthorizationStatus.NotDetermined => \(status == CLAuthorizationStatus.NotDetermined)")
+        
+        
+        if status == CLAuthorizationStatus.NotDetermined {
+            
+            //debug
+            print("[\(Methods.basename(__FILE__)):\(__LINE__)] starting... => request dialog")
+            
+            // 未認証ならリクエストダイアログ出す
+            locationManager.requestWhenInUseAuthorization();
+        }
+        
+        //debug
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] starting... => locationManager")
+        
+        
+        // 位置情報の取得を開始させる
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+
+        //debug
+        print("[\(Methods.basename(__FILE__)):\(__LINE__)] map setup => done")
+	
+    }
     
     // 入力画面から戻ってきた時に TableView を更新させる
     override func viewWillAppear(animated: Bool) {
@@ -38,7 +92,15 @@ class VC_ShowLoc: UIViewController {
         //debug
         print("[\(Methods.basename(__FILE__)):\(__LINE__)] self.loc?.description => \(self.loc?.description)")
         
+        // set pin
+        self._viewWillAppear__SetPin()
         
     }
 
+    func _viewWillAppear__SetPin() {
+    
+        
+        
+    }
+    
 }
